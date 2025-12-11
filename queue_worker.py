962c9383816cb22aec5c4090b1a5425014a37247
f8k6dm2m6f-collab <f8k6dm2m6f@privaterelay.appleid.com>
2025-12-11 23:15:57 +0100

    Create queue_worker.py

---
+++ queue_worker.py
@@ -0,0 +1,24 @@
+import asyncio
+from compressor import compress_video
+from audio import extract_audio, normalize_audio
+
+queue = asyncio.Queue()
+
+async def worker():
+    while True:
+        task = await queue.get()
+        type_ = task["type"]
+        src = task["src"]
+        out = task["out"]
+
+        if type_ == "compress":
+            compress_video(src, out)
+        elif type_ == "audio":
+            extract_audio(src, out)
+        elif type_ == "normalize":
+            normalize_audio(src, out)
+
+        queue.task_done()
+
+async def start_worker():
+    asyncio.create_task(worker())
